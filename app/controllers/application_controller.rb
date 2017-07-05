c54dd0b036b4e546b4df705d875978b7c5e854c6
algoregalore <cemurray88@gmail.com>
2017-07-05 09:32:15 -0500

    setup sessions controller, current user helper and sessions routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # helper methods available to controller
+  helper_method :current_user
+
+  private
+  # if session has user id, then find user, make them current user
+  def current_user
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+  end
 end
